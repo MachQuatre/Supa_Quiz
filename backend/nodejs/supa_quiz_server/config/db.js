962a4ad921e3f68ae5b04b87ff3af78ea6e1d79e
MachQuatre <mbenmammar.mpi23@eleve-irup.com>
2025-03-13 15:18:21 +0100

    [FIX] Création BDD et lancement

--- backend/nodejs/supa_quiz_server/config/db.js
+++ backend/nodejs/supa_quiz_server/config/db.js
@@ -1,13 +1,17 @@
-const mongoose = require("mongoose");
 require("dotenv").config();
+const mongoose = require("mongoose");
+
+const mongoURI = process.env.MONGO_URI;
+
+if (!mongoURI) {
+    console.error("❌ Erreur: La variable MONGO_URI est undefined !");
+    process.exit(1);
+}
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log("✅ MongoDB connecté !");
+        await mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log("✅ Connexion à MongoDB réussie !");
     } catch (error) {
         console.error("❌ Erreur de connexion MongoDB:", error);
         process.exit(1);
