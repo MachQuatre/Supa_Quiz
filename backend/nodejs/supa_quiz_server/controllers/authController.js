5172210838e88d568b23c6ae84d32e17f462066c
MachQuatre <mbenmammar.mpi23@eleve-irup.com>
2025-03-13 14:50:35 +0100

    [INIT] architecture v1

---
+++ backend/nodejs/supa_quiz_server/controllers/authController.js
@@ -0,0 +1,43 @@
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const crypto = require("crypto");
+
+exports.signup = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+
+        const existingUser = await User.findOne({ email });
+        if (existingUser) return res.status(400).json({ message: "Email déjà utilisé." });
+
+        const newUser = new User({ 
+            user_id: crypto.randomUUID(),
+            username,
+            email,
+            password 
+        });
+
+        await newUser.save();
+        res.status(201).json({ message: "Inscription réussie !" });
+    } catch (error) {
+        res.status(500).json({ message: "Erreur serveur", error });
+    }
+};
+
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+
+        if (!user) return res.status(400).json({ message: "Utilisateur non trouvé." });
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(401).json({ message: "Mot de passe incorrect." });
+
+        const token = jwt.sign({ user_id: user.user_id, role: user.role }, process.env.JWT_SECRET, { expiresIn: "7d" });
+
+        res.json({ message: "Connexion réussie", token });
+    } catch (error) {
+        res.status(500).json({ message: "Erreur serveur", error });
+    }
+};
