9ea6ac5b42c1fe7f93c3c027eee6ebc53fb33d7c
MachQuatre <mbenmammar.mpi23@eleve-irup.com>
2025-03-13 14:50:35 +0100

    [INIT] architecture v1

--- backend/nodejs/supa_quiz_server/config/db.js
+++ backend/nodejs/supa_quiz_server/config/db.js
@@ -1,18 +1,17 @@
-// config/db.js
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectDB = async () => {
-  try {
-    const uri = process.env.MONGODB_URI || 'mongodb://10.9.5.123:27017/quiz_app';
-    const conn = await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB connecté sur', conn.connection.host);
-  } catch (error) {
-    console.error('Erreur de connexion à MongoDB:', error);
-    process.exit(1);
-  }
+    try {
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("✅ MongoDB connecté !");
+    } catch (error) {
+        console.error("❌ Erreur de connexion MongoDB:", error);
+        process.exit(1);
+    }
 };
 
 module.exports = connectDB;
