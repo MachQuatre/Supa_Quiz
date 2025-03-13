373b87f35d5ff4b122a9490092d339968995c7a1
RemiCOSTET <rcostet.mpi23@eleve-irup.com>
2025-03-13 15:04:55 +0100

    [ADD app.js]

---
+++ backend/nodejs/supa_quiz_server/app.js
@@ -0,0 +1,14 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+
+const app = express();
+app.use(express.json());
+
+connectDB();
+
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`🚀 Serveur démarré sur le port ${PORT}`));
