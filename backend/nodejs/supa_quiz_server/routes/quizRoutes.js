c7e22f1ff81112fa0b309dd72e31717552a76de1
MachQuatre <mbenmammar.mpi23@eleve-irup.com>
2025-05-13 16:05:00 +0200

    [IMP] Go fini côté back

--- backend/nodejs/supa_quiz_server/routes/quizRoutes.js
+++ backend/nodejs/supa_quiz_server/routes/quizRoutes.js
@@ -6,9 +6,11 @@ const roleMiddleware = require("../middlewares/roleMiddleware");
 
 router.get("/", quizController.getAllQuizzes);
 router.get("/mine", authMiddleware, quizController.getMyQuizzes);
-router.get("/:quizId", quizController.getQuizById);
 router.post("/", authMiddleware, roleMiddleware("admin", "super_user"), quizController.createQuiz);
 router.put("/:quizId", authMiddleware, roleMiddleware("admin", "super_user"), quizController.updateQuiz);
 router.delete("/:quizId", authMiddleware, roleMiddleware("admin", "super_user"), quizController.deleteQuiz);
+router.get("/:quizId/questions", authMiddleware, quizController.getQuizQuestions);
+router.delete("/:quizId/questions/:questionId", authMiddleware, roleMiddleware("admin", "super_user"), quizController.deleteQuizQuestion);
+router.get("/:quizId", quizController.getQuizById);
 
 module.exports = router;
