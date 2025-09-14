6570096f724115d2d71faf7f296a676a2cbafa83
MachQuatre <mbenmammar.mpi23@eleve-irup.com>
2025-09-14 17:48:03 +0200

    [ADD] Bouton admin

--- frontend/flutter/supa_quizz_app/lib/features/home/screens/home_screen.dart
+++ frontend/flutter/supa_quizz_app/lib/features/home/screens/home_screen.dart
@@ -4,7 +4,8 @@ import '../../home/screens/login_screen.dart';
 import '../../leaderboard/screens/leaderboard_screen.dart';
 import '../../profile/screens/profile_screen.dart';
 import '../../quiz/screens/play_screen.dart';
-import '../../../widgets/admin_shortcut_button.dart'; // ⬅️ ajout
+import '../../../widgets/admin_shortcut_button.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -32,11 +33,18 @@ class _HomeScreenState extends State<HomeScreen> {
         backgroundColor: Colors.black,
         actions: [
           // ⚙️ Raccourci vers l'interface Admin (même onglet)
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 4),
-            child: AdminShortcutButton(compact: true),
+          IconButton(
+            icon: const Icon(Icons.settings_suggest_rounded),
+            tooltip: 'Admin',
+            onPressed: () async {
+              final uri = Uri.parse('/admin/login'); // même domaine (nginx)
+              await launchUrl(
+                uri,
+                mode: LaunchMode.platformDefault,
+                webOnlyWindowName: '_self', // même onglet
+              );
+            },
           ),
-
           // 🎓 Bouton Entrainement
           IconButton(
             icon: const Icon(Icons.school),
